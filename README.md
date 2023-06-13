8635ba59592c46e648bd66fe8ed77e14ea3aa559
NjinuParam <njinu@webparam.org>
2023-06-13 09:30:11 +0200

    feat(vercel)

--- README.md
+++
@@ -1,5 +0,0 @@
-# med-ts-node
-
-This is the source code for my tutorial on medium
-
-[Article link](https://medium.com/swlh/typescript-with-mongoose-and-node-express-24073d51d2ee)
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,27 @@
-import express from 'express';
+import express, { Request, Response } from 'express'
+// import express from 'express';
 import mongoose from 'mongoose'
 import { json, urlencoded } from 'body-parser';
 import { userRouter } from './routes/user'
 import { personnelRouter } from './routes/personnel';
 const formData = require("express-form-data");
 const os = require("os");
-const app = express()
+ const app = express()
 const options = {
   uploadDir: os.tmpdir(),
   autoClean: true
 };
 app.use(json())
-// app.use(formData.parse(options));
-// // delete from the request all empty files (size == 0)
-// app.use(formData.format());
-// // change the file objects to fs.ReadStream 
-// app.use(formData.stream());
-// // union the body and the files
-// app.use(formData.union());
 app.use(userRouter, personnelRouter)
+// const app = express()
+const port = process.env.PORT || 8080
 
-mongoose.connect("mongodb+srv://suntecTMS:suntectms2022@cluster0.zm9cv.mongodb.net/viconet?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-}, () => {
-  console.log('connected to database')
+app.get('/', (_req: Request, res: Response) => {
+	return res.send('Viconet_BE running on port 8080')
 })
 
-app.listen(3000, () => {
-  console.log('server is listening on port 3000')
+mongoose.connect("mongodb+srv://suntecTMS:suntectms2022@cluster0.zm9cv.mongodb.net/viconet?retryWrites=true&w=majority")
+
+app.listen(port, () => {
+	return console.log(`Server is listening on ${port}`)
 })
